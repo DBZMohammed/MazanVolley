72421d4db3cc7074c1f6a82124d5ef098dc6e90f
Momo Dbiza <mohammed.dbiza@gmail.com>
2019-09-02 14:58:30 +0200

    Automatic commit - PushToGithub Command

--- resources/views/infos.blade.php
+++ resources/views/infos.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<form class="col-md-6">
+<div class="container">
+
+
+<form class="col-md-12 justify-content-center">
     <div class="form-group row">
       <label for="text" class="col-md-3 col-form-label">Text :</label>
-      <div class="col-md-9">
+      <div class="col-md-6">
         <input type="text" class="form-control" id="text">
       </div>
     </div>
     <div class="form-group row">
       <label for="textarea" class="col-md-3 col-form-label">Textarea :</label>
-      <div class="col-md-9">
+      <div class="col-md-6">
         <input type="textarea" class="form-control" id="textarea">
       </div>
     </div>
     <div class="form-group row">
       <label for="select" class="col-md-3 form-control-label">Select :</label>
-      <div class="col-md-9">
+      <div class="col-md-6">
         <select id="select" class="form-control" >
           <option>Option 1</option>
           <option>Option 3</option>
@@ -26,11 +29,11 @@
     </div>
     <div class="form-group row">
       <div class="col">
-        <button type="submit" class="btn">Envoyer</button>
+        <button type="submit" class="btn btn-success">Envoyer</button>
       </div>
     </div>
   </form>
-
+</div>
 @endsection
 
 
